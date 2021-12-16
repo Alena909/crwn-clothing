1766a2cb5894330165933c762fc64b17645d40de
Yelena Guthrie <yelena.guthrie@gmail.com>
2021-12-15 20:45:29 -0600

    added collection items

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./page/homepage/homepage.component";
+import ShopPage from "./page/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
