b4b074f5448f2eec1873ad7c22c1681a2357338f
Yelena Guthrie <yelena.guthrie@gmail.com>
2022-01-11 20:54:19 -0600

    updated stripe integration and updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KGu1rDo5jJZ3HI4CRvNV8S2b9xCbVxGEIAvSTlcPzEmARwYkZALPadKATcOhd43wbpWt40zbUkW3lPA2b4XncVl00VHh9eaAI";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
